48d9a64ccfe60e4bdd8e293707259432612cf01c
Rakesh singh <rakeshsingh@Rakeshs-MacBook-Pro.local>
2023-08-28 11:31:10 -0700

    changes to run the batch job each time we start our application

--- spring-batch-ex/src/main/java/com/batch/SpringBatchExApplication.java
+++ spring-batch-ex/src/main/java/com/batch/SpringBatchExApplication.java
@@ -1,14 +1,22 @@
 package com.batch;
 
+import java.util.UUID;
 import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameter;
+import org.springframework.batch.core.JobParametersBuilder;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -22,9 +30,22 @@ public class SpringBatchExApplication {
 	}
 
   @Bean
-  Tasklet tasklet(){
+  ApplicationRunner runner(JobLauncher jobLauncher, Job job){
+    return args -> {
+      var jobParameters =
+          new JobParametersBuilder()
+              .addString("uniqueId", UUID.randomUUID().toString())
+              .toJobParameters();
+      var run = jobLauncher.run(job, jobParameters);
+      var instanceId = run.getJobInstance().getInstanceId();
+      System.out.println("instance id is : "+instanceId);
+    };
+  }
+  @Bean
+  @StepScope
+  Tasklet tasklet(@Value("#{jobParameters['uniqueId']}") String uniqueId){
     return (contribution, chunkContext) -> {
-      System.out.println("Hello world");
+      System.out.println("Hello world uniqueId = "+uniqueId);
       return RepeatStatus.FINISHED;
     };
   }
